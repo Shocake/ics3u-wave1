749daeafb10af127b67c95c450fc02edd98fcd05
Justin Xing <justinxing3@gmail.com>
2020-04-11 14:18:23 -0400

    added file

---
+++ area.py
@@ -0,0 +1,5 @@
+length = float(input("Input the length of the field in feet."))
+width = float(input("Input the width of the field in feet."))
+area0 = length * width
+area = str(area0 / 43560)
+print("The area of the field is " + area + " acres")
